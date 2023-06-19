d54d5f59b344e2ece61bb47c5be4866ec50735cd
uygarkaya <uygar.kaya@avl.com>
2023-06-19 21:51:07 +0300

    new package was created

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/uygarkaya/ros/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/uygarkaya/ros/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/uygarkaya/ros/catkin_ws/devel;/opt/ros/melodic' != '' else []

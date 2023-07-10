6551de4fd9031c7f610575af474a89bbdf970535
uygarkaya <uygar.kaya@avl.com>
2023-07-10 20:03:09 +0300

    parallel simulation was added

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,11 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/uygarkaya/ros/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/uygarkaya/ros/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/melodic/bin:/home/uygarkaya/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/:/mnt/c/WINDOWS/System32/OpenSSH/:/mnt/c/Program Files/SafeNet/Authentication/SAC/x64:/mnt/c/Program Files/SafeNet/Authentication/SAC/x32:/mnt/c/Program Files/Microsoft VS Code/bin:/mnt/c/Users/u28i18/.dnx/bin:/mnt/c/Program Files/Microsoft DNX/Dnvm/:/mnt/c/Program Files/Microsoft SQL Server/130/Tools/Binn/:/mnt/c/Program Files (x86)/Windows Kits/10/bin/10.0.17763.0/x64/rc.exe:/mnt/c/Program Files (x86)/MATLAB/MATLAB Compiler Runtime/v713/runtime/win32:/mnt/c/Program Files/dotnet/:/mnt/c/Program Files/Git/cmd:/mnt/c/Users/u28i18/AppData/Local/Programs/Python/Python310/Scripts/:/mnt/c/Users/u28i18/AppData/Local/Programs/Python/Python310/:/mnt/c/Users/u28i18/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/u28i18/.dotnet/tools:/snap/bin'
+export PKG_CONFIG_PATH="/home/uygarkaya/ros/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/uygarkaya/ros/catkin_ws/build'
+export PYTHONPATH="/home/uygarkaya/ros/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/uygarkaya/ros/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/uygarkaya/ros/catkin_ws/src:$ROS_PACKAGE_PATH"
--- catkin_ws/build/vision/catkin_generated/installspace/publisher.py
+++ catkin_ws/build/vision/catkin_generated/installspace/publisher.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python2
-import rospy, cv2, time
-
+import rospy, cv2, time, multiprocessing
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 bridge = CvBridge()
 pTime = 0
-LANE_PATH = rospy.get_param('/image_publisher_/path')
-MULTILANE_PATH = rospy.get_param('/image_publisher/path')
+counter = 0
 
-def publish_image():
-    global pTime
+def publish_image(path, topic="vision/image"):
+    global pTime, counter
 
-    rospy.init_node('vision_node')
-    rospy.loginfo('Publisher Node has been started...')
-
-    publisher = rospy.Publisher('vision/image', Image, queue_size=1)
+    publisher = rospy.Publisher(topic, Image, queue_size=1)
     rate = rospy.Rate(30)
-    cap = cv2.VideoCapture(MULTILANE_PATH)
+    cap = cv2.VideoCapture(path)
 
     while not rospy.is_shutdown():
         success, img = cap.read()
+        # rospy.loginfo("{} - {} detected...".format(counter, topic))
+        # counter = counter + 1
         if not success:
+            rospy.loginfo("{} - {} detected...".format(counter, topic))
+            counter = counter + 1
             cap.release()
-            cap = cv2.VideoCapture(MULTILANE_PATH)
+            cap = cv2.VideoCapture(path)
             continue
+            # break
         
         cTime = time.time()
         fps = 1/(cTime-pTime)
@@ -34,6 +34,7 @@ def publish_image():
 
         image_msg = bridge.cv2_to_imgmsg(img, "bgr8")
         publisher.publish(image_msg)
+        log(topic=topic, msg=image_msg) # log the image msg
 
         rate.sleep()
 
@@ -43,9 +44,41 @@ def publish_image():
         if rospy.is_shutdown():
             cap.release()
 
+def parallel_run():    
+    process1 = multiprocessing.Process(target=publish_image, args=(rospy.get_param('/image_publisher/path1'), 'vision/image1',))
+    process2 = multiprocessing.Process(target=publish_image, args=(rospy.get_param('/image_publisher/path2'), 'vision/image2',))
+    
+    process1.start()
+    process2.start()
+
+    process1.join()
+    process2.join()
+
+def non_parallel_run():
+    publish_image(path=rospy.get_param('/image_publisher/path1'))
+    rospy.loginfo('vision/image1 topic was published, now vision/image2 start to publish...')
+    publish_image(path=rospy.get_param('/image_publisher/path2'))
+
+
+def log(topic, msg):
+    try:
+        with open('/logs/msg.txt', 'w') as file_:
+            file_.write("Topic: {} - MSG:{}".format(topic, msg))
+    except FileNotFoundError:
+        print("The 'logs' directory does not exist")
+
 
 if __name__ == '__main__':
     try:
-        publish_image()
+        start_time = time.time()
+
+        rospy.init_node('vision_node')
+        rospy.loginfo('publisher node has been started...')
+        
+        parallel_run()
+        # non_parallel_run()
+
+        end_time = time.time()
+        rospy.loginfo("Execution Time: {}".format(end_time-start_time))
     except rospy.ROSInterruptException:
         pass
